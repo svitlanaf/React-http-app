2f12aaa4dbfe80528b0a03e9abf9d7698e5d5152
Svitlana Filatova <svitlana.filatova.gmail.com>
2019-07-10 12:54:48 -0700

    Add tracking errors using Sentry

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from "react";
+import * as Sentry from "@sentry/browser";
 import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+// import Raven from "raven-js";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
+
+Sentry.init({
+  dsn: "https://d1ad25a155504627808740cf87b187bf@sentry.io/1501997"
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
--- src/services/httpService.jsx
+++ src/services/httpService.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import * as Sentry from "@sentry/browser";
 import { toast } from "react-toastify";
 
 axios.interceptors.response.use(null, error => {
@@ -7,7 +8,8 @@ axios.interceptors.response.use(null, error => {
     error.response.status >= 400 &&
     error.response.status < 500;
   if (!expectedError) {
-    console.log("Logging the error", error);
+    Sentry.captureException(error);
+    // console.log("Logging the error", error);
     toast.error("An unexpected error occured.");
   }
   return Promise.reject(error);
